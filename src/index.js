2598de464ddb93d3e39125f0ae44f8efaec1a7df
Andrew <rakitinandrei@gmail.com>
2023-02-06 14:19:41 +0300

    init: add decoding ability

--- src/index.js
+++ src/index.js
@@ -38,7 +38,34 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    const length = expr.length;
+    const numberOfsigns = length / 10;
+    let cloneOfExpr;
+    const arrayOfLetters = [];
+    for (let i = 0; i < numberOfsigns; i++) {
+        cloneOfExpr = expr;
+        newLetter = cloneOfExpr.slice(0 + i * 10, 10 + i * 10);
+        arrayOfLetters.push(newLetter);
+    }
+    const modifyedArray = arrayOfLetters.map( (item) => {
+        if (item === '**********') {
+            return ' '
+        } else {return String(item * 1)}
+    })
+    const arrayWithSymbols = modifyedArray.map( (item) => {
+        if (item === ' ') {
+            return ' '
+        } else {return item.replaceAll('10', '.').replaceAll('11', '-')}
+    })
+    const arrayWithLetters = arrayWithSymbols.map( (item) => {
+        if (item === ' ') {
+            return ' '
+        } else {
+            return MORSE_TABLE[item]
+        }
+    })
+    const result = arrayWithLetters.join('');
+    return result;
 }
 
 module.exports = {
